c99138534cc7dde761c93b4fcf5f876810fac3e9
YevhenSoldatov1998 <arr.ess1998@gmail.com>
2020-02-02 16:19:20 +0200

    work4 commit

--- js/common.js
+++ js/common.js
@@ -2,5 +2,26 @@ $(function() {
 $('.hamburger').click(function(){
     $('.menu').toggleClass('active');
 })
-
+$('.accordion_body').slideUp()
+$('.accordion_head').click(function () {
+    $(this).toggleClass('active')
+    $(this).next().slideToggle()
+})
+    $('.close, .overlay').click(function () {
+        $('.popup').addClass('popup-hide')
+    })
+    $('.menu >div').click(function () {
+        $('.menu').removeClass('active')
+    })
+//    top button
+    $('.show-popup-one').click(function () {
+        $('.popup-one').removeClass('popup-hide')
+    })
+    //    first-section button
+    $('.show-popup-two').click(function () {
+        $('.popup-two').removeClass('popup-hide')
+    })
+    $('.show-popup-three').click(function () {
+        $('.popup-three').removeClass('popup-hide')
+    })
 });
