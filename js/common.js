1a22c6274d4a67d687ee431a242303dcbc675ca7
YevhenSoldatov1998 <arr.ess1998@gmail.com>
2020-02-03 08:13:37 +0200

    last commit

--- js/common.js
+++ js/common.js
@@ -1,6 +1,8 @@
 $(function() {
+
 $('.hamburger').click(function(){
     $('.menu').toggleClass('active');
+    
 })
 $('.accordion_body').slideUp()
 $('.accordion_head').click(function () {
@@ -10,6 +12,7 @@ $('.accordion_head').click(function () {
     $('.close, .overlay').click(function () {
         $('.popup').addClass('popup-hide')
     })
+
     $('.menu >div').click(function () {
         $('.menu').removeClass('active')
     })
@@ -24,4 +27,13 @@ $('.accordion_head').click(function () {
     $('.show-popup-three').click(function () {
         $('.popup-three').removeClass('popup-hide')
     })
+    var $root = $('html, body');
+
+    $('a[href^="#"]').click(function () {
+        $root.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+
+        return false;
+    });
 });
